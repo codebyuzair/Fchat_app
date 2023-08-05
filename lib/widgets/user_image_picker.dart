caaa8d5425595a0e4f49575f5a0a94512b639c35
codebyuzair <uzairmanagi@gmail.com>
2023-08-05 23:41:21 +0500

    flutter chat_app

---
+++ lib/widgets/user_image_picker.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({
+    super.key,
+    required this.onPickImage,
+  });
+
+  final void Function(File pickedImage) onPickImage;
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File? _pickImageFile;
+
+  void _pickImage() async {
+    final pickedImage = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      imageQuality: 50,
+      maxWidth: 150,
+    );
+    setState(() {
+      if (pickedImage == null) {
+        return;
+      }
+      _pickImageFile = File(pickedImage.path);
+    });
+
+    widget.onPickImage(_pickImageFile!);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 40,
+          backgroundColor: Colors.grey,
+          foregroundImage:
+              _pickImageFile != null ? FileImage(_pickImageFile!) : null,
+        ),
+        TextButton.icon(
+          onPressed: _pickImage,
+          icon: const Icon(Icons.image),
+          label: Text(
+            'Add Image',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
